ef42d04959b326849fe8c9773ab23802573ad407
Crypto Minion <154598612+jrwbabylonlab@users.noreply.github.com>
2024-11-30 14:32:58 +1100

    feat: expose incentive query and tx (#19)
    * feat: expose incentive query and tx

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * as btcstakingtx from './generated/babylon/btcstaking/v1/tx';
 export * as btcstaking from './generated/babylon/btcstaking/v1/btcstaking';
 export * as btcstakingpop from './generated/babylon/btcstaking/v1/pop';
 export * as btccheckpoint from './generated/babylon/btccheckpoint/v1/btccheckpoint';
+export * as incentivetx from './generated/babylon/incentive/tx';
+export * as incentivequery from './generated/babylon/incentive/query';
