71071996dfc23952394191b51b08d7473d9d9bab
wjrjerome <wjrjerome@babylonchain.io>
2024-12-11 21:33:39 +1100

    feat: expose btclightclient query

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * as btcstakingpop from './generated/babylon/btcstaking/v1/pop';
 export * as btccheckpoint from './generated/babylon/btccheckpoint/v1/btccheckpoint';
 export * as incentivetx from './generated/babylon/incentive/tx';
 export * as incentivequery from './generated/babylon/incentive/query';
+export * as btclightclient from './generated/babylon/btclightclient/v1/btclightclient';
+export * as btclightclientquery from './generated/babylon/btclightclient/v1/query';
