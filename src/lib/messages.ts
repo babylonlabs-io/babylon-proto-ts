48c702f7b2bb2f84165d4faf1b2df6e234957c07
David Totraev <totraev@gmail.com>
2025-07-08 18:52:54 +0500

    feat: decompose babylon client

---
+++ src/lib/messages.ts
@@ -0,0 +1,17 @@
+import * as incentivetx from "../generated/babylon/incentive/tx";
+import { BTC_STAKER } from "../utils/constants";
+import { REGISTRY_TYPE_URLS } from "../utils/registry";
+
+export const createMessages = () => ({
+  createWithdrawRewardMsg(address: string) {
+    const withdrawRewardMsg = incentivetx.MsgWithdrawReward.fromPartial({
+      type: BTC_STAKER,
+      address,
+    });
+
+    return {
+      typeUrl: REGISTRY_TYPE_URLS.MsgWithdrawReward,
+      value: withdrawRewardMsg,
+    };
+  },
+});
